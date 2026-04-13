c4eb5e1132289a800810915f833dd8a950b01a28
Benjamin Malley <1026445+BenjaminMalley@users.noreply.github.com>
2026-04-13 06:31:39 -0700

    [nvim] add nvim-autopairs

--- nvim/init.lua
+++ nvim/init.lua
@@ -140,6 +140,19 @@ require("lazy").setup({
     end,
   },
 
+  -- Auto-pairing
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- Integrate with nvim-cmp
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
+
   -- Java Support (Specialized)
   { "mfussenegger/nvim-jdtls" },
 }, {
